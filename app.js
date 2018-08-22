d98182e4e73b705bd667a0828a1d8404fc6baa9d
Uy Binh <uybinh@gmail.com>
2018-08-22 22:10:24 +0700

    Change MainGame to IIFE

--- app.js
+++ app.js
@@ -37,50 +37,45 @@ const Players = (function(){
 })()
 
 /**
+ * @module GameBoard
+ * @returns .status(), .getCell(), .setCell()
+ */
+const GameBoard = (function(){
+  let boardStatus = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
+
+  return {
+    status(){
+      return boardStatus
+    },
+    getCell(posX, posY){
+      return boardStatus[posX][posY]
+    },
+    setCell(posX, posY, value){
+      if (this.getCell(posX, posY) !== value) {
+        boardStatus[posX][posY] = value
+        return boardStatus[posX][posY]
+      } else {
+        return false
+      }
+    },
+    checkWin(){
+
+    }
+  }
+})()
+
+
+/**
  * Represent the main module of the Game
  * @module MainGame
  * @returns .GameBoard
  */
-const MainGame = function(){
-  /**
-   * @module GameBoard
-   * @returns .status(), .getCell(), .setCell()
-   */
-  const GameBoard = (function(){
-    let boardStatus = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
-    ]
+const MainGame = (function(){
 
-    return {
-      status(){
-        return boardStatus
-      },
-      getCell(posX, posY){
-        return boardStatus[posX][posY]
-      },
-      setCell(posX, posY, value){
-        if (this.getCell(posX, posY) !== value) {
-          boardStatus[posX][posY] = value
-          return boardStatus[posX][posY]
-        } else {
-          return false
-        }
-      },
-      checkWin(){
-
-      }
-    }
-  })()
-
-  /**
-   * Below belong to MainGame
-   */
-
-  /**
-   * Set cells value
-   */
   const refreshBoard = function(){
     let cells = document.querySelectorAll('.cell')
     cells.forEach((cell) => {
@@ -88,10 +83,7 @@ const MainGame = function(){
       cell.dataset.value = GameBoard.getCell(posX, posY)
     })
   }
-  refreshBoard()
 
-  Players.add('Binh','x')
-  Players.add('Xuan','o')
   /**
    * Add events to cells
    */
@@ -108,17 +100,15 @@ const MainGame = function(){
       })
     })
   }
-  setCellsEvent()
 
-  return { GameBoard, Players }
-}
+  const start = function(){
+    refreshBoard()
+    Players.add('Binh','x')
+    Players.add('Xuan','o')
+    setCellsEvent()
+  }
 
-/**
- * Create new game after DOM loaded
- */
-// document.addEventListener('DOMContentLoaded', ()=>{
-//   let game = MainGame()
-//   console.log(game)
-// })
+  return { start }
+})()
 
-let game = MainGame()
+let game = MainGame.start()
